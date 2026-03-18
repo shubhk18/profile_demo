9e0d00d816c67a02c46c66bd616e832d071de863
shubham KUSHWAHA <shubhk18@users.noreply.github.com>
2026-03-18 23:50:26 +0530

    Remove httplib.h download from start.sh
    Removed download command for httplib.h in start.sh.

--- start.sh
+++ start.sh
@@ -5,7 +5,6 @@ echo "🔧 Installing compiler..."
 apt-get update
 apt-get install -y g++
 
-curl -O https://raw.githubusercontent.com/yhirose/cpp-httplib/master/httplib.h
 
 echo "🔧 Building server..."
 g++ server.cpp -std=c++17 -O2 -pthread -o server
