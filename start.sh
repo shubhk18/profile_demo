67298b18c5545227f0d72ea73fba03f6adf3b746
shubham KUSHWAHA <shubhk18@users.noreply.github.com>
2026-03-18 23:46:19 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 set -e
 
+echo "🔧 Installing compiler..."
+apt-get update
+apt-get install -y g++
+
+echo "🔧 Building server..."
+g++ server.cpp -std=c++17 -O2 -pthread -o server
+
+echo "📂 Files:"
+ls -l
+
 echo "🚀 Starting server..."
-
-ls -l   # debug
-
 ./server
